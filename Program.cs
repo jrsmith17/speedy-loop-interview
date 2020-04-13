57db1f01b0d4b17d7dbf03eaef68e29527102f47
JR Smith <jrsmith17@gmail.com>
2020-04-12 21:19:16 -0400

    Testing reading in file line by line.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace whitespace {
+    class Program {
+        static void Main(string[] args) {
+            string line;  
+            
+            // Read the file and display it line by line.  
+            StreamReader file = new StreamReader(System.IO.Path.Combine(Environment.CurrentDirectory, "input.txt"));
+            while((line = file.ReadLine()) != null){  
+                System.Console.WriteLine(line);  
+            }  
+            
+            file.Close();  
+        }
+    }
+}
